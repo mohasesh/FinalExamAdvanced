0681be58c0d86b3c29b83d312edda5d0e05750fe
Mohammed Adem <mohata1234@gmail.com>
2021-01-18 20:11:16 -0500

    first commit

---
+++ FinalExamAdvanced/GenericApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalExamAdvanced
+{
+   public  class GenericApp<T> where T:IEnumerable
+    {
+        private T mylist { get; set; }
+        GenericApp<ArrayList> m = new GenericApp<ArrayList>();
+        
+    }
+}
--- FinalExamAdvanced/Program.cs
+++ FinalExamAdvanced/Program.cs
@@ -6,7 +6,30 @@ namespace FinalExamAdvanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            lightcheker light = new lightcheker();
+            light.ProcessCompleted += Lightcheck; 
+            light.StartProcess();
+        }
+        public static void Lightcheck(object sender, EventArgs e)
+        {
+            Console.WriteLine("The Light is Turned on");
+        }
+    }
+    public class lightcheker
+    {
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+
+            OnProcessCompleted(EventArgs.Empty);
+        }
+
+
+        protected virtual void OnProcessCompleted(EventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
         }
     }
 }
